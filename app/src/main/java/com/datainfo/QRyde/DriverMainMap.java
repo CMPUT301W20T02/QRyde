048b1f79be406eee88bda28d1ffcadbc93cd5f95
atonyma7 <atonyma7@gmail.com>
2020-02-16 22:52:48 -0700

    added a basic sliding panel

---
+++ app/src/main/java/com/datainfo/QRyde/DriverMainMap.java
@@ -0,0 +1,14 @@
+package com.datainfo.QRyde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverMainMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_main_map);
+    }
+}
