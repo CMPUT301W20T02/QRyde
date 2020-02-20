d0e7a1336d201518bed3062fed20e7b8d45b8e9d
kangxi11 <rkang2009@gmail.com>
2020-02-20 13:58:08 -0700

    doing ride requests

--- app/src/main/java/com/example/qryde/tempActivity.java
+++ app/src/main/java/com/example/qryde/tempActivity.java
@@ -10,5 +10,7 @@ public class tempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+
+
     }
 }
