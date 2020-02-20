09a545a403c3e38d9f1a41ef66fe18c4e304129a
atonyma7 <atonyma7@gmail.com>
2020-02-20 11:10:08 -0700

    added some fragments

--- app/src/main/java/com/datainfo/QRyde/DriverMainMap.java
+++ app/src/main/java/com/datainfo/QRyde/DriverMainMap.java
@@ -2,6 +2,7 @@ package com.datainfo.QRyde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,26 +15,29 @@ import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class DriverMainMap extends AppCompatActivity {
+public class DriverMainMap extends AppCompatActivity implements WaitingUserResponseFragment.OnFragmentInteractionListener{
 
     AvailableRideAdapter rideAdapter;
     ArrayList<AvailableRide> dataList;
+    EditText startLocationEditText;
+    EditText endLocationEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driver_main_map);
 
         ListView availableRideListView = findViewById(R.id.list_view);
-        final EditText startLocationEditText = findViewById(R.id.start_location_et);
-        final EditText endLocationEditText = findViewById(R.id.end_location_et);
+        startLocationEditText = findViewById(R.id.start_location_et);
+        endLocationEditText = findViewById(R.id.end_location_et);
 
 
-        AvailableRide [] AvailableRideList = {
-                new AvailableRide("Masahiro Sakurai", "ur mom", "ur dad", 20.6f, 0.7f),
-                new AvailableRide("Hideo Kojima", "ur mom", "ur dad", 17.8f, 1.3f),
-                new AvailableRide("Todd Howard", "ur mom", "ur dad", 27.7f, 1.7f),
-                new AvailableRide("Sean Murray", "ur mom", "ur dad", 30.8f, 1.9f),
-                new AvailableRide("Reggie Fils-Aime", "ur mom", "ur dad", 52.9f, 2.3f)
+        AvailableRide[] AvailableRideList = {
+                new AvailableRide("Masahiro Sakurai", "Nintendo", "Super Smash Bros", 20.6f, 0.7f),
+                new AvailableRide("Reggie Fils-Aime", "Nintendo", "Wii Fit", 52.9f, 2.3f),
+                new AvailableRide("Hideo Kojima", "Kojima Productions", "Metal Gear Solid", 17.8f, 1.3f),
+                new AvailableRide("Todd Howard", "Bethesda Softworks", "Fallout 76", 27.7f, 1.7f),
+                new AvailableRide("Sean Murray", "Hello Games", "No Mans Sky", 30.8f, 1.9f),
         };
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(AvailableRideList));
@@ -46,13 +50,27 @@ public class DriverMainMap extends AppCompatActivity {
         availableRideListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
-                startLocationEditText.setText(dataList.get(position).getStartLocation());
-                endLocationEditText.setText(dataList.get(position).getEndLocation());
+                waitForUser(position);
                 return true;
             }
 
         });
 
     }
+    public void waitForUser(int position){
+        final SlidingUpPanelLayout slidingUpPanelLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_panel);
+        slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
+        startLocationEditText.setText(dataList.get(position).getStartLocation());
+        endLocationEditText.setText(dataList.get(position).getEndLocation());
+        WaitingUserResponseFragment newFrag = new WaitingUserResponseFragment();
+        newFrag.show(getSupportFragmentManager(), "WAITING_USER_RESPONSE");
+    }
+
+    public void onCanceledPressed(){
+        finish();
+        overridePendingTransition( 0, 0);
+        startActivity(getIntent());
+        overridePendingTransition( 0, 0);
+    }
+
 }
---
+++ app/src/main/java/com/datainfo/QRyde/RideInProgressFragment.java
@@ -0,0 +1,50 @@
+package com.datainfo.QRyde;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class RideInProgressFragment extends DialogFragment {
+
+    private OnFragmentInteractionListener listener;
+
+    public interface OnFragmentInteractionListener {
+        void onCanceledPressed();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener){
+            listener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.user_response_fragment, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder
+                .setTitle("Ride in progress...")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        listener.onCanceledPressed();
+                    }}).create();
+    }
+}
