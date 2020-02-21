92f3a1df07637d1269546a16a6fae72fdb1cc4eb
Judalopez28 <lopez.juandavid28@gmail.com>
2020-02-20 17:01:10 -0700

    changing location of qrscanner

--- app/src/main/java/com/example/qryde/ScanQrcode.java
+++ app/src/main/java/com/example/qryde/ScanQrcode.java
@@ -1,4 +1,4 @@
-package com.datainfo.QRyde;
+package com.example.qryde;
 
 import android.Manifest;
 import android.content.Context;
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.datainfo.QRyde.R;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
