84f51b7316fdd42d1f00a12f4c5ea92144b3c01c
Ryan Kang <rkang2009@gmail.com>
2020-03-12 18:41:24 -0600

    firebase to robotium

--- app/src/androidTest/java/com/example/qryde/MainActivityTest.java
+++ app/src/androidTest/java/com/example/qryde/MainActivityTest.java
@@ -1,5 +1,8 @@
 package com.example.qryde;
 
+import android.app.Activity;
+import android.widget.EditText;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -11,6 +14,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
 /**
  * Test class for MainActivity. All the UI tests are written here. Robotium test framework is
  used
@@ -18,14 +26,40 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest{
     private Solo solo;
+    private FirebaseFirestore db;
+
     @Rule
     public ActivityTestRule<MainActivity> rule =
             new ActivityTestRule<>(MainActivity.class, true, true);
     @Before
     public void setUp() throws Exception{
         solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+        db = FirebaseFirestore.getInstance();
     }
+
     @Test
-    public void checkCiyListItem(){
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+    @Test
+    public void signupActivity() throws InterruptedException {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+//        solo.clickOnButton("Login");
+        solo.clickOnText("Signup");
+        solo.assertCurrentActivity("Wrong Activity", signup.class);
+        solo.enterText((EditText) solo.getView(R.id.name_edittext), "Test User's Name");
+        solo.enterText((EditText) solo.getView(R.id.phone_edittext), "12345678");
+        solo.enterText((EditText) solo.getView(R.id.username_edittext), "testUsername");
+        solo.enterText((EditText) solo.getView(R.id.email_edittext), "test@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.password_edittext), "password");
+        solo.enterText((EditText) solo.getView(R.id.reenterPassword_edittext), "password");
+        solo.clickOnButton("Signup");
+
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+
+
+
     }
 }
