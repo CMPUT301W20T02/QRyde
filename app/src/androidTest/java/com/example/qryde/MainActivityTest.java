9473e1893b8e5f2ec6aa7571bb332c392f3779c9
atonyma7 <atonyma7@gmail.com>
2020-03-12 18:17:11 -0600

    robotium 3.6

--- app/src/androidTest/java/com/example/qryde/MainActivityTest.java
+++ app/src/androidTest/java/com/example/qryde/MainActivityTest.java
@@ -1,4 +1,30 @@
 package com.example.qryde;
 
-public class MainActivityTest {
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+/**
+ * Test class for MainActivity. All the UI tests are written here. Robotium test framework is
+ used
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest{
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
 }
