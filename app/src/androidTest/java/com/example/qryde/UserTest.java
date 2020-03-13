61f9f400f5d1f36c6fc83044facf7f5d572c9f76
Ryan Kang <rkang2009@gmail.com>
2020-03-13 11:04:35 -0600

    finished user tests

--- app/src/androidTest/java/com/example/qryde/UserTest.java
+++ app/src/androidTest/java/com/example/qryde/UserTest.java
@@ -14,9 +14,13 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.robotium.solo.Solo;
 
 import org.junit.Before;
@@ -24,10 +28,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
-
+import static junit.framework.TestCase.fail;
 
 
 /**
@@ -54,8 +61,8 @@ public class UserTest {
         Activity activity = rule.getActivity();
     }
 
-    @Test
-    public void riderSignUpTest() throws InterruptedException{
+//    @Test
+    public void requestRideThenCancel() throws InterruptedException{
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
         solo.enterText((EditText) solo.getView(R.id.username_edittext), "bigtodd");
         solo.enterText((EditText) solo.getView(R.id.password_edittext), "123");
@@ -72,10 +79,160 @@ public class UserTest {
 
         View view = solo.getView(R.id.qryde_logo);
         solo.clickOnView(view);
+        solo.enterText((EditText) solo.getView(R.id.amount), "25");
+        solo.clickOnButton("confirm");
 
-        solo.sleep(10000);
+        solo.sleep(500);
+
+        db.collection("AvailableRides")
+                .whereEqualTo("rider", "bigtodd")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            assertTrue(true);
+                        } else {
+                            fail();
+                        }
+                    }
+                });
+
+        solo.sleep(500);
+
+        db.collection("AvailableRides").document("bigtodd")
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "onSuccess: Successfully deleted document");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: Failed to delete document");
+                    }
+                });
+
+        solo.sleep(2000);
+
+        solo.clickOnButton("cancel");
 
 
+    }
+
+    @Test
+    public void requestRideComplete() throws InterruptedException{
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.username_edittext), "bigtodd");
+        solo.enterText((EditText) solo.getView(R.id.password_edittext), "123");
+        solo.clickOnButton("Login");
+        solo.assertCurrentActivity("Wrong Activity", MapActivity.class);
+
+        solo.clickOnScreen(766,424);
+        solo.sendKey(KeyEvent.KEYCODE_E);
+        solo.sendKey(KeyEvent.KEYCODE_T);
+        solo.sendKey(KeyEvent.KEYCODE_L);
+        solo.sendKey(KeyEvent.KEYCODE_C);
+
+        solo.clickOnScreen(744,430);
+
+        View view = solo.getView(R.id.qryde_logo);
+        solo.clickOnView(view);
+        solo.enterText((EditText) solo.getView(R.id.amount), "25");
+        solo.clickOnButton("confirm");
+
+        solo.sleep(500);
+
+        db.collection("AvailableRides")
+                .whereEqualTo("rider", "bigtodd")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            assertTrue(true);
+                        } else {
+                            fail();
+                        }
+                    }
+                });
+
+        solo.sleep(500);
+
+        // update firebase so that next action occurs
+        db.collection("AvailableRides").document("bigtodd")
+                .update("driver", "driver")
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "onSuccess: Successfully deleted document");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: Failed to delete document");
+                    }
+                });
+        db.collection("AvailableRides").document("bigtodd")
+                .update("status", "true")
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "onSuccess: Successfully deleted document");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: Failed to delete document");
+                    }
+                });
+        solo.sleep(500);
+
+        solo.clickOnButton("confirm");
+
+        solo.sleep(2000);
+
+        // check that firebase updated successfully
+        db.collection("ActiveRides")
+                .whereEqualTo("rider", "bigtodd")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            assertTrue(true);
+                        } else {
+                            fail();
+                        }
+                    }
+                });
+
+        solo.sleep(500);
+
+        solo.clickOnButton("Ride Complete");
+        solo.assertCurrentActivity("Wrong Activity", GenerateQRCode.class);
+
+        db.collection("ActiveRides").document("bigtodd")
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "onSuccess: Successfully deleted document");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: Failed to delete document");
+                    }
+                });
+
+
+        solo.sleep(2000);
 
 
     }
