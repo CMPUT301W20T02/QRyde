e623258db956c4a39ff8034043d625c3bf7b54b3
Mustafa Khairullah <mustafa.khairullah@gmail.com>
2020-03-27 15:47:33 -0600

    Adding user feedback for drivers

--- app/src/main/java/com/example/qryde/RideComplete.java
+++ app/src/main/java/com/example/qryde/RideComplete.java
@@ -1,9 +1,11 @@
 package com.example.qryde;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,6 +30,10 @@ public class RideComplete extends AppCompatActivity {
     private String amountOfferedString;
     private TextView amountOfferedTv;
 
+    private ImageButton thumbsUp;
+    private ImageButton thumbsDown;
+
+    private boolean positive = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +42,6 @@ public class RideComplete extends AppCompatActivity {
         Bundle incomingData = getIntent().getExtras();
         ScanButton = findViewById(R.id.scan_qr_button);
         amountOfferedTv = findViewById(R.id.qr_amount_text);
-
 
         if (incomingData != null) {
             user = incomingData.getString("username");
@@ -58,6 +63,33 @@ public class RideComplete extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), ScanQRCode.class);
                 intent.putExtra("username", user);
                 startActivity(intent);
+            }
+        });
+
+        thumbsUp = findViewById(R.id.thumbsUpButton);
+        thumbsDown = findViewById(R.id.thumbsDownButton);
+
+        thumbsUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /**
+             * This method allows the user to give the driver positive feedback
+             * after the ride is over
+             * @param View
+             */
+            public void onClick(View v) {
+                //positive will stay true
+            }
+        });
+
+        thumbsDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /**
+             * This method allows the user to give the driver negative feedback
+             * after the ride is over
+             * @param View
+             */
+            public void onClick(View v) {
+                positive = false;
             }
         });
     }
