40d25cb1a946c4ddd762d06a5c154778835d51c6
Mayowa Lajide <lajide@ualberta.ca>
2020-03-29 15:43:59 -0700

    User Stories 3.03.01. Allows user click on username and see the other users contact info

--- app/src/main/java/com/example/qryde/MapActivity.java
+++ app/src/main/java/com/example/qryde/MapActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
